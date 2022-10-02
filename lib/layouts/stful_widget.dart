d8d8ecb44c35f3dedcefa9465eeef40105593be0
Nathan Luk <nathan.luk@heykit.com.au>
2022-10-02 11:04:47 +1100

    organisation of files

---
+++ lib/layouts/stful_widget.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class FavouriteWidget extends StatefulWidget {
+  const FavouriteWidget({super.key});
+
+  @override
+  State<FavouriteWidget> createState() => _FavouriteWidgetState();
+}
+
+class _FavouriteWidgetState extends State<FavouriteWidget> {
+  bool _isFavourited = true;
+  int _favouriteCount = 41;
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavourited) {
+        _isFavourited = !_isFavourited;
+        _favouriteCount -= 1;
+        return;
+      }
+
+      _isFavourited = !_isFavourited;
+      _favouriteCount += 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(0),
+          child: IconButton(
+            padding: const EdgeInsets.all(0),
+            alignment: Alignment.centerRight,
+            icon: (_isFavourited
+                ? const Icon(Icons.star)
+                : const Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: SizedBox(
+            child: Text('$_favouriteCount'),
+          ),
+        ),
+      ],
+    );
+  }
+}
